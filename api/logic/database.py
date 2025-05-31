e80c460f9cfb105313b94af6249817b88a8dab19
Mihail <168080573+ExShare074@users.noreply.github.com>
2025-05-31 11:30:26 +0300

    Расширен учёт калорий до БЖУ + ККал

--- api/logic/database.py
+++ api/logic/database.py
@@ -32,18 +32,29 @@ def user_exists(username: str) -> bool:
 def is_completed(username: str):
     return users[username]["plan"].is_completed()
 
-# ==== КАЛОРИИ ====
+# ==== КАЛОРИИ И БЖУ ====
 
-def add_calories(username: str, product: str, kcal: int):
+def add_calories(username: str, product: str, protein: int, fat: int, carbs: int, kcal: int):
     today = str(date.today())
     user = users[username]
     if today not in user["calories"]:
         user["calories"][today] = []
-    user["calories"][today].append({"product": product, "kcal": kcal})
+    user["calories"][today].append({
+        "product": product,
+        "protein": protein,
+        "fat": fat,
+        "carbs": carbs,
+        "kcal": kcal
+    })
 
 
 def get_today_calories(username: str):
     today = str(date.today())
     entries = users[username]["calories"].get(today, [])
-    total = sum(e["kcal"] for e in entries)
+    total = {
+        "protein": sum(e["protein"] for e in entries),
+        "fat": sum(e["fat"] for e in entries),
+        "carbs": sum(e["carbs"] for e in entries),
+        "kcal": sum(e["kcal"] for e in entries)
+    }
     return {"total": total, "entries": entries}
